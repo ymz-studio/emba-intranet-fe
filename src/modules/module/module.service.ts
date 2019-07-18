2fe7ebe2b6ad039fc054732cfa59529745f1a26e
wsq <mywsq961@gmail.com>
2019-07-18 23:49:47 +0800

    chore: add module schedule calendar component

--- src/modules/module/module.service.ts
+++ src/modules/module/module.service.ts
@@ -1,3 +1,15 @@
+import { AxiosDefault } from "@/plugins/axios";
+import { ModuleScheduleItem, ModuleInfo } from "./module.interfaces";
+
 export class ModuleService {
-  static async getModuleInfoNow() {}
+  // 获取当前的module信息
+  static async getModuleInfoNow(): Promise<ModuleInfo> {
+    const { data } = await AxiosDefault.get("/api/module/now");
+    return data;
+  }
+  // 获取全部module日程安排
+  static async getModuleSchedule(): Promise<ModuleScheduleItem[]> {
+    const { data } = await AxiosDefault.get("/api/module/schedule");
+    return data;
+  }
 }
--- src/modules/module/module.store.ts
+++ src/modules/module/module.store.ts
@@ -3,20 +3,48 @@ import {
   ModuleInfo,
   ModuleScheduleItem
 } from "@/modules/module/module.interfaces";
+import { ModuleService } from "./module.service";
+import AppStore from "@/store";
 
 export const ModuleMutations = {
   SET_CUR_MODULE: "Set currently module info [module]",
   SET_SCHEDULE: "Set module schedule info [module]"
 };
 
-export const MiduleActions = {
-  SET_CUR_MODULE: "SET_CUR_MODULE",
-  SET_SCHEDULE: "SET_SCHEDULE"
+export const ModuleActions = {
+  LOAD_CUR_MODULE: "LOAD_CUR_MODULE",
+  LOAD_SCHEDULE: "LOAD_SCHEDULE"
 };
 
 export const ModuleStore = new Vuex.Store({
   state: {
     curModule: undefined as ModuleInfo | undefined,
     schedule: undefined as ModuleScheduleItem[] | undefined
+  },
+  mutations: {
+    // 设置日程表信息
+    [ModuleMutations.SET_SCHEDULE](state, payload) {
+      state.schedule = payload;
+    },
+    // 设置当前模块信息
+    [ModuleMutations.SET_CUR_MODULE](state, payload) {
+      state.schedule = payload;
+    }
+  },
+  actions: {
+    async [ModuleActions.LOAD_SCHEDULE](ctx) {
+      const schedule = await ModuleService.getModuleSchedule();
+      ctx.commit(ModuleMutations.SET_SCHEDULE, schedule);
+    },
+    async [ModuleActions.LOAD_CUR_MODULE](ctx) {
+      const module = await ModuleService.getModuleInfoNow();
+      ctx.commit(ModuleMutations.SET_CUR_MODULE, module);
+    }
   }
 });
+
+function init() {
+  AppStore.registerModule("module", ModuleStore);
+}
+
+init();
