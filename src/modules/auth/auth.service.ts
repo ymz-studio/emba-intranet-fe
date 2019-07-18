10e2d35cfd26fc8132d94cde8160424ac8c36419
myWsq <wsq961@outlook.com>
2019-07-18 18:37:58 +0800

    chore(auth module): 完善部分逻辑

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,18 +1,28 @@
-import { LoginPayload } from "@/modules/auth/auth.interfaces";
+import { LoginPayload, CurUserInfo } from "@/modules/auth/auth.interfaces";
+import { AxiosDefault } from "@/plugins/axios";
 
 export class AuthService {
   /**
    * 获取token
    * @param payload 登录信息
    */
-  static async login(payload: LoginPayload) {
-    return "";
+  static async login(payload: LoginPayload): Promise<CurUserInfo> {
+    const { data } = await AxiosDefault.post("/api/auth", payload);
+    return data;
+  }
+
+  /**
+   * 清除用户cookie中的登录信息
+   */
+  static async logout() {
+    await AxiosDefault.delete("/api/auth");
   }
 
   /**
    * 已登录用户获取自身信息
    */
-  static async auth() {
-    return {};
+  static async auth(): Promise<CurUserInfo> {
+    const { data } = await AxiosDefault.get("/api/auth");
+    return data;
   }
 }
